10916b438ed747240b503eb1ae1677d2ea32a3e8
swatii23 <swati.m_2000@rediffmail.com>
2024-03-26 15:45:28 +0530

    score updates

--- game.js
+++ game.js
@@ -2,16 +2,59 @@
 const number1 = document.getElementById("number1");
 const number2 = document.getElementById("number2");
 
+var score = 0;
+
 function generateNumber() {
-    return Math.ceil(Math.random() * 100);
+    number1.textContent = Math.ceil(Math.random() * 100);
+    number2.textContent = Math.ceil(Math.random() * 100);    
 }
 
-number1.textContent = generateNumber();
-number2.textContent = generateNumber();
 
 
 // Iteration 3: Make the options button functional
+const greater = document.getElementById("greater-than");
+const equal = document.getElementById("equal-to");
+const lesser = document.getElementById("lesser-than");
 
+greater.onclick = () => {
+    // console.log(`${number1.textContent} ${number2.textContent}`)
+    if(number1.textContent > number2.textContent)
+    score++;
+    console.log(score)
+    generateNumber()
+}
+
+equal.onclick = () => {
+    // console.log(`${number1.textContent} ${number2.textContent}`)
+    if(number1.textContent == number2.textContent)
+    score++;
+    console.log(score)
+    generateNumber()
+}
+
+lesser.onclick = () => {
+    // console.log(`${number1.textContent} ${number2.textContent}`)
+    if(number1.textContent < number2.textContent)
+    score++;
+    console.log(score)
+    generateNumber()
+}
 
 
 // Iteration 4: Build a timer for the game
+
+let timeInSeconds = 10;
+const timerElement = document.getElementById("timer");
+
+function updateTimer() {
+  timerElement.textContent = timeInSeconds + "s";
+  if(timeInSeconds > 0) {
+    timeInSeconds--;
+  } else {
+    window.location.href = "gameover.html"
+  }
+}
+
+setInterval(updateTimer, 1000);
+
+export { score }
--- score.js
+++ score.js
@@ -1,1 +1,5 @@
 // Iteration 5: Store the player score and display it on the game over screen
+import { score } from "./game";
+
+const scoreBoard = document.getElementById("score-board");
+scoreBoard.textContent = score;

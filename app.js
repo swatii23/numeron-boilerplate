3d85c94e5ec56d41e5b419965487961272718e1b
swatii23 <swati.m_2000@rediffmail.com>
2024-03-26 15:06:53 +0530

    functionality of play button and random num generation

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // Iteration 1: Make the Play button functional.
 // Description: When the Play button is clicked the game page should be displayed.
+
+const playBtn = document.getElementById("play-button");
+playBtn.onclick = () => {
+    window.location.href = "game.html"
+}
--- game.js
+++ game.js
@@ -1,5 +1,17 @@
 // Iteration 2: Generate 2 random number and display it on the screen
+const number1 = document.getElementById("number1");
+const number2 = document.getElementById("number2");
+
+function generateNumber() {
+    return Math.ceil(Math.random() * 100);
+}
+
+number1.textContent = generateNumber();
+number2.textContent = generateNumber();
+
 
 // Iteration 3: Make the options button functional
+
+
 
 // Iteration 4: Build a timer for the game

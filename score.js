6dc7ea4537974213dee6a9f2f67e6777ee6f3f82
swatii23 <swati.m_2000@rediffmail.com>
2024-03-27 08:52:48 +0530

    play again button functionality

--- score.js
+++ score.js
@@ -1,3 +1,7 @@
 // Iteration 5: Store the player score and display it on the game over screen
 const scoreBoard = document.getElementById("score-board");
 scoreBoard.textContent = JSON.parse(localStorage.getItem("score"));
+
+document.getElementById("play-again-button").onclick = () => {
+    window.location.href = "index.html"
+}
